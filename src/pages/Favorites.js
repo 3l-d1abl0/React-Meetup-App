ea7e03dce6954d4b12dece7f5b004b879bda5cb3
sameer <sameer.barha12@gmail.com>
2021-12-13 22:43:03 +0530

    working favorites Page

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,6 +1,24 @@
+import {useContext} from 'react';
+
+import FavoriteContext from '../store/favorites-context';
+import MeetupList from '../components/meetups/MeetupList';
+
 function FavoritesPage(){
 
-    return <div>Favourite Meetups !</div>
+    const favoriteCtx = useContext(FavoriteContext);
+
+    let content;
+
+    if(favoriteCtx.totalFavorites === 0){
+        content = <p> Ypu got favorites yet. start adding some ?</p>
+    }else{
+        content = <MeetupList meetups={favoriteCtx.favorites} />
+    }
+
+    return <section>
+            <h1>Favourite Meetups !</h1>
+                {content}
+            </section>
 }
 
 
